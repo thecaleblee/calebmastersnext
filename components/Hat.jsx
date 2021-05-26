79f0e7cde434d0b0794211753010a97839ce6689
Caleb Masters <calebmasters@gmail.com>
2021-05-26 17:23:04 -0500

    Updating to match new requirements for meta

--- components/Hat.jsx
+++ components/Hat.jsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 
-export default function Hat (props = '') {
+export default function Hat (props = {}) {
 
   const {
+    title = '',
     currentPage = 'Home',
   } = props;
 
   return (
     <Head>
-      <title>Caleb Masters - Front End Engineer Portfolio - {currentPage}</title>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta http-equiv="Content-Type" content="text/html; charset= ISO-8859-1" />
+      <title>{title} - {currentPage}</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="Content-Type" content="text/html; charSet= ISO-8859-1" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Engineer Portfolio for Caleb Masters. Front-End Engineer, UX promotor, and Hobby Collector" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
